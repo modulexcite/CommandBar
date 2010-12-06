ea091ea61084c19cef16930d7bb38fb54e9f318f
unknown <lutzr@.redmond.corp.microsoft.com>
2010-12-06 06:52:50 -0800

    fix build script

--- Tools/Build.bat
+++ Tools/Build.bat
@@ -6,5 +6,13 @@ set FrameworkPath=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319
 if exist "%FrameworkPath%\csc.exe" goto :Start
 :start
 
-%FrameworkPath%\csc.exe /target:library /out:..\Build\CommandBar.dll *.cs /keyfile:KeyPair.snk %1
+if exist ..\Build rd /q /s ..\Build
+md ..\Build
 
+pushd ..\Source
+%FrameworkPath%\csc.exe /target:library /out:..\Build\CommandBar.dll *.cs /keyfile:KeyPair.snk %1
+popd
+
+pushd ..\Example
+%FrameworkPath%\csc.exe /target:winexe /out:..\Build\Example.exe *.cs /r:..\Build\CommandBar.dll /res:ImageList16.png /res:ImageList24.png
+popd
