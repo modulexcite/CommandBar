0663187cc5bb77b1be4086c73617a5a6052b3b4c
lutzroeder <lutzr@.redmond.corp.microsoft.com>
2010-12-06 04:03:50 -0800

    remove example build.bat

--- Example/Build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set FrameworkPath=%SystemRoot%\Microsoft.NET\Framework\v2.0.50727
-if exist "%FrameworkPath%\csc.exe" goto :start
-set FrameworkPath=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319
-if exist "%FrameworkPath%\csc.exe" goto :start
-:start
-
-%FrameworkPath%\csc.exe /target:exe /out:..\Build\HelloWorld.exe *.cs /r:..\Build\CommandBar.dll %1 /res:ImageList16.png /res:ImageList24.png
